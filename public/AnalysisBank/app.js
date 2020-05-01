f20dd5d8672a600ad84709daa4e66b8052b0fa90
Rohan Kumar <rrohankumar2012@gmail.com>
2020-05-01 16:51:02 +0530

    Added Blood bank analysis prediction, Removed Display

--- public/AnalysisBank/app.js
+++ public/AnalysisBank/app.js
@@ -6,8 +6,7 @@ $(document).ready(function(){
     var predVal = document.getElementById('predict')
     var resultsIDName = []
     var resultRecieved = false;
-    btn.onclick=function(){
-        
+    btn.onclick=function(){        
         CheckBloodCamp();
         function CheckBloodCamp(){
             console.log("BloodBank")
@@ -44,8 +43,15 @@ $(document).ready(function(){
                 }
             })
         }
+
+        let x = document.getElementById("allbb");
+        x.style.display = "block";
     }
 
+
+    var myAgeChart;
+    var myGenderChart;
+    var myBloodGpChart;  
 
     let age = document.getElementById('age')
     age.onclick = function(){
@@ -53,8 +59,8 @@ $(document).ready(function(){
             alert("Enter info If entered then no results found")
             return;
         }
-        // let x = document.getElementById("displayAgeGraph");
-        // x.style.display = "block";
+        let x = document.getElementById("displayAgeGraph");
+        x.style.display = "block";
         let AgeGrp = [10,20,30,40,50]
         let AgeCount = [0,0,0,0,0]
         let AgeJsonObj = {}
@@ -77,10 +83,39 @@ $(document).ready(function(){
                 })
             }
         }
+
         function DisplayAge(){
             for(let i=0;i<AgeGrp.length;i++)
             AgeJsonObj[AgeGrp[i]]=AgeCount[i]
             console.log("Age Count Here ",AgeJsonObj)
+
+            if (myAgeChart) {
+                myAgeChart.destroy();
+            }
+
+            var ctx = document.getElementById('ageBarGraph').getContext('2d');
+            myAgeChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ['10-20', '20-30', '30-40', '40-50', 'Above 50'],
+                    datasets: [{
+                        label: 'Number of donors in different age groups',
+                        data: AgeCount,
+                        backgroundColor: [
+                            '#003f5c','#58508d','#bc5090','#ff6361','#ffa600'
+                        ],
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
         }
     }
     
@@ -91,8 +126,8 @@ $(document).ready(function(){
             alert("Enter info If entered then no results found")
             return;
         }
-        // let x = document.getElementById("displayGenderGraph");
-        // x.style.display = "block";
+        let x = document.getElementById("displayGenderGraph");
+        x.style.display = "block";
         let GenderGrp = ['M','F']
         let GenderCount = [0,0]
         let GenderJsonObj = {}
@@ -120,9 +155,27 @@ $(document).ready(function(){
             for(let i=0;i<GenderGrp.length;i++)
             GenderJsonObj[GenderGrp[i]]=GenderCount[i]
             console.log("Gender Count Here ",GenderJsonObj)
+
+            if (myGenderChart) {
+                myGenderChart.destroy();
+            }
+
+            var ctx = document.getElementById('genderPieChart').getContext('2d');
+            myGenderChart = new Chart(ctx, {
+                type: 'pie',
+                data: {
+                    labels: ['Male','Female'],
+                    datasets: [{
+                        label: 'Number of donors gender wise',
+                        data: GenderCount,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 1)','rgba(54, 162, 235, 1)'
+                        ],
+                    }]
+                },
+            });
         }
     }
-
 
 
     let BloodGrp = document.getElementById('bloodGrp')
@@ -131,8 +184,8 @@ $(document).ready(function(){
             alert("Enter info If entered then no results found")
             return;
         }
-        // let x = document.getElementById("displayBloodGpGraph");
-        // x.style.display = "block";
+        let x = document.getElementById("displayBloodGpGraph");
+        x.style.display = "block";
         let BgGrp = ["O+","O-","B-","B+","AB+","AB-"]
         let BgCount = [0,0,0,0,0,0]
         let BgJsonObj = {}
@@ -159,7 +212,36 @@ $(document).ready(function(){
         function DisplayBg(){
             for(let i=0;i<BgGrp.length;i++)
             BgJsonObj[BgGrp[i]]=BgCount[i]
-            console.log("Age Count Here ",BgJsonObj)
+            console.log("Blood group Count Here ",BgJsonObj)
+
+            if (myBloodGpChart) {
+                myBloodGpChart.destroy();
+            }
+
+            var ctx = document.getElementById('bloodGrpBarGraph').getContext('2d');
+            myBloodGpChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: BgGrp,
+                    datasets: [{
+                        label: 'Number of different blood group donors',
+                        data: BgCount,
+                        backgroundColor: [
+                            '#ffa600','#ff6361','#bc5090','#36a2eb','#58508d','#003f5c'
+                        ],
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
         }
     }
+
 })
